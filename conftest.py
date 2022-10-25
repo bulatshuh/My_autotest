14c9ec33a3053948a4953059c63f4adb2865f2d4
bulatshuh <Bulat.Khisamutdinov@icl-services.com>
2022-10-25 19:13:32 +0300

    correcting conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from fake_useragent import UserAgent
+# from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
@@ -11,19 +10,14 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='function')
 def browser(request):
-    user_agent = UserAgent()
     browser_name = request.config.getoption('browser')
     browser = None
     if browser_name == 'chrome':
         print('\nOpening Chrome browser...')
-        # options = Options()
-        # options.add_argument(f'user-agent={user_agent.random}')
         browser = webdriver.Chrome()
     elif browser_name == 'firefox':
         print('\nOpening Firefox browser...')
-        options = webdriver.FirefoxOptions()
-        options.set_preference('general.useragent.override', user_agent.random)
-        browser = webdriver.Firefox(options=options)
+        browser = webdriver.Firefox()
     else:
         raise pytest.UsageError('--browser should be chrome or firefox')
     yield browser
