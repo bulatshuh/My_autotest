3e7be159d8b2e40a269a4ff2685642fb51227b2e
bulatshuh <Bulat.Khisamutdinov@icl-services.com>
2022-11-10 19:11:09 +0300

    add test for switching to next and previous reel

--- test_shorts_page.py
+++ test_shorts_page.py
@@ -2,7 +2,7 @@ from .pages.shorts_page import ShortsPage
 import pytest
 
 
-@pytest.mark.new
+@pytest.mark.smoke
 def test_user_can_watch_reels(browser):
     link = 'https://www.youtube.com/'
     page = ShortsPage(browser, link)
@@ -10,4 +10,18 @@ def test_user_can_watch_reels(browser):
     page.go_to_shorts_page()
     page.go_full_screen()
     page.reel_container_appear()
+    page.email_field_not_appear()
+
+
+@pytest.mark.new
+@pytest.mark.flaky(reruns=5)
+def test_switch_next_and_previous_reel(browser):
+    link = 'https://www.youtube.com/'
+    page = ShortsPage(browser, link)
+    page.open()
+    page.go_to_shorts_page()
+    page.go_full_screen()
+    page.switch_to_next_reel()
+    page.email_field_not_appear()
+    page.switch_to_previous_reel()
     page.email_field_not_appear()
